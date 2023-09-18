865ba6386df834d48f4f4ed1d3477bcf4021c20e
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2023-09-18 14:51:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import altair as alt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-# Definir función para obtener datos desde Google Sheets
+# Función para obtener datos desde Google Sheets
 def get_data_from_gsheets(sheet_url):
     # Credenciales y autenticación
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets',
@@ -22,7 +25,7 @@ def get_data_from_gsheets(sheet_url):
     return df
 
 # Iniciar la app Streamlit
-st.title('Análisis Exploratorio de Datos desde Google Sheets')
+st.title('Análisis Exploratorio de Datos y Clustering desde Google Sheets')
 
 # Cargar los datos
 url = "https://docs.google.com/spreadsheets/d/1r4YcJuh5Qvp9_Z9D4soEyZymZD6tGTYBqqevXTIT6AQ/edit#gid=0"
@@ -34,26 +37,40 @@ except Exception as e:
     st.write("Hubo un error al cargar los datos.")
     st.write(e)
 
-# Si los datos se cargaron, visualizar un histograma
+# Si los datos se cargaron, realizar el clustering y visualización
 if 'data' in locals():
     metrics = ['ROE', 'ROA', 'EBITDA', 'APALANCAMIENTO', 'ACTIVOS', 'PASIVOS', 'PATRIMONIO', 
                'INGRESOS DE ACTIVIDADES ORDINARIAS', 'GANANCIA BRUTA', 'GANANCIA (PÉRDIDA) POR ACTIVIDADES DE OPERACIÓN', 'GANANCIA (PÉRDIDA)']
     
-    column_to_plot = st.selectbox('Selecciona una métrica para visualizar', metrics)
-    if pd.api.types.is_numeric_dtype(data[column_to_plot]):
-        histogram = alt.Chart(data).mark_bar().encode(
-            alt.X(column_to_plot, bin=True),
-            y='count()',
-        ).properties(
-            width=600,
-            height=400
-        )
-        st.altair_chart(histogram)
-    else:
-        st.write(f"La métrica {column_to_plot} no es numérica y no puede visualizarse como histograma.")
+    st.subheader('Clustering basado en métricas financieras')
+    num_clusters = st.slider("Selecciona el número de clusters", 2, 10, 3)
+    
+    # 1. Preprocesamiento
+    df_metrics = data[metrics].dropna()  # Eliminar cualquier fila con datos faltantes
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(df_metrics)
+    
+    # 2. Modelo KMeans
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    df_metrics['cluster'] = kmeans.fit_predict(scaled_data)
+    
+    # 3. Visualización
+    pca = PCA(2)
+    pca_result = pca.fit_transform(scaled_data)
+    df_metrics['pca_1'] = pca_result[:,0]
+    df_metrics['pca_2'] = pca_result[:,1]
+    
+    chart = alt.Chart(df_metrics).mark_circle(size=60).encode(
+        x='pca_1',
+        y='pca_2',
+        color='cluster:N',
+        tooltip=['ROE', 'ROA', 'EBITDA']
+    ).interactive()
+    st.altair_chart(chart)
 
-# Instrucciones para ejecutar:
+# Para ejecutar el código:
 # 1. Guarda este código en un archivo, por ejemplo "app.py".
-# 2. En tu terminal o línea de comandos, navega al directorio donde guardaste el archivo.
+# 2. Instala las dependencias con pip.
 # 3. Ejecuta el comando: streamlit run app.py
+
 
