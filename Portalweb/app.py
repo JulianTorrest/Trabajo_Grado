13b8b605d86b2b4f4e9a518fcd44c60cac5d8c90
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2023-09-27 23:28:33 -0500

    Update app.py

--- Portalweb/app.py
+++ Portalweb/app.py
@@ -34,6 +34,7 @@ def main():
         "JulianTorrest/Trabajo_Grado/main/Portalweb/Colombia/Agricultura,ganaderia,caza,silvicultura,pesca.png",
         "JulianTorrest/Trabajo_Grado/main/Portalweb/Colombia/Producción de carne de res,pollo y cerdo.png",
         "JulianTorrest/Trabajo_Grado/main/Portalweb/Colombia/Actividad pecuaria.png",
+        "JulianTorrest/Trabajo_Grado/main/Portalweb/Colombia/Producción y variación en principales actividades agroindustriales.png",
     ]
     
     for path in paths:
